3ec2996fff9c59f6175099792c9f5f15c083b4c7
sbowman96 <sbowman96@gmail.com>
2017-06-04 09:29:52 -0500

    heroku db

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGOBD_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.export = {mongoose};
